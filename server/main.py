64546b82edaa443dfa47ef59a2460cbf650aceba
benj210003 <bjj35@pitt.edu>
2025-01-29 10:07:44 -0500

    Finished basic crud api

--- server/main.py
+++ server/main.py
@@ -41,3 +41,33 @@ def get_stories():
     conn.close()
 
     return [StoryRead(id=story[0], title=story[1], story=story[2]) for story in rows]
+
+@app.put("/stories/{story_id}")
+def edit_story(story_id: int, story_data: StoryCreate):
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE stories SET title = ?, story = ? WHERE id = ?",
+        (story_data.title, story_data.story, story_id,)
+    )
+
+    conn.commit()
+    conn.close
+
+    return StoryRead(id=story_id, title=story_data.title, story=story_data.title)
+
+@app.delete("/stories/{story_id}")
+def delete_story(story_id: int):
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM stories WHERE id = ?",
+        (story_id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return {"message": f"Story {story_id} deleted succesfully"}
